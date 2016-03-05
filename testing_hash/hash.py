0df3e4600c146a74d10605f0af17e68c3b57199a
xov17 <vrmallory@gmail.com>
2016-03-06 00:00:40 +0800

    New tests

--- testing_hash/hash.py
+++ testing_hash/hash.py
@@ -1,7 +1,8 @@
 import hashlib
-mystring = raw_input(b'Enter String to hash: ')
+mystring = raw_input('Enter String to hash: ')
 # Assumes the default UTF-8
-hash_object = hashlib.md5(mystring.encode())
+#hash_object = hashlib.md5(mystring.encode())
+
 print(hash_object.hexdigest())
 print 'Hash of "hello world"'
 print(hashlib.md5(b'Hello World').hexdigest())
